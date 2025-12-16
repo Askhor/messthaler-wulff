4075616c9f179cf47bf3d4b256781283e28fb90d
guenthner <j@guenthner.xyz>
2025-12-16 06:34:45 +0100

    minimisers now no longer counts crystals multiple times

--- messthaler_wulff/minimiser_simulation.py
+++ messthaler_wulff/minimiser_simulation.py
@@ -1,6 +1,5 @@
 import logging
 import math
-from dataclasses import dataclass
 
 from .additive_simulation import OmniSimulation
 from .simulation_state import CrystalHasher, AdvancedSimulation
@@ -8,33 +7,38 @@ from .simulation_state import CrystalHasher, AdvancedSimulation
 log = logging.getLogger("messthaler_wulff")
 
 
-@dataclass(frozen=True)
-class Data:
-    min_energy: int
-    count: int
+def hacky_cache(cache_name):
+    def deco(function):
+        def impl(self, n: int):
+            cache = getattr(self, cache_name)
+            while len(cache) < n + 1:
+                cache.append(function(self, len(cache)))
+
+            return cache[n]
+
+        return impl
+
+    return deco
 
 
 class MinimiserSimulation:
     def __init__(self, omni_simulation: OmniSimulation, hasher: CrystalHasher):
         self.sim = AdvancedSimulation(omni_simulation, hasher)
 
-        self.cache = [Data(0, 1)]
+        self.min_energy_cache = [0]
+        self.count_cache = [1]
         self.minimisers_cache = [(self.sim.initial_state,)]
 
-    def calculate_data(self, n: int) -> Data:
-        min_energy = math.inf
-        count = 0
+    def probable_minimisers(self, n):
+        if n <= 0:
+            raise ValueError(f"n must be positive ({n})")
 
-        for state in self.probable_minimisers(n):
-            if state.energy < min_energy:
-                count = 1
-                min_energy = state.energy
-            elif state.energy == min_energy:
-                count += 1
+        for state in self.minimisers(n - 1):
+            for atom in state.next_atoms:
+                yield state.add_atom(atom)
 
-        return Data(min_energy, count)
-
-    def calculate_minimisers(self, n: int) -> tuple:
+    @hacky_cache("minimisers_cache")
+    def minimisers(self, n):
         min_energy = self.min_energy(n)
         minimisers = []
         visited = set()
@@ -48,31 +52,17 @@ class MinimiserSimulation:
 
         return tuple(minimisers)
 
-    def get_data(self, n) -> Data:
-        while len(self.cache) < n + 1:
-            self.cache.append(self.calculate_data(len(self.cache)))
-
-        return self.cache[n]
-
-    def probable_minimisers(self, n):
-        if n <= 0:
-            raise ValueError(f"n must be positive ({n})")
-
-        for state in self.minimisers(n - 1):
-            for atom in state.next_atoms:
-                yield state.add_atom(atom)
-
-    def minimisers(self, n):
-        if n < 0:
-            raise ValueError(f"n must be non-negative ({n})")
-
-        while len(self.minimisers_cache) < n + 1:
-            self.minimisers_cache.append(self.calculate_minimisers(len(self.minimisers_cache)))
-
-        return self.minimisers_cache[n]
-
+    @hacky_cache("min_energy_cache")
     def min_energy(self, n):
-        return self.get_data(n).min_energy
+        min_energy = math.inf
 
+        for state in self.probable_minimisers(n):
+            if state.energy < min_energy:
+                min_energy = state.energy
+
+        return min_energy
+
+    @hacky_cache("count_cache")
     def minimiser_count(self, n: int):
-        return self.get_data(n).count
+
+        return len(self.minimisers(n))
--- messthaler_wulff/version.py
+++ messthaler_wulff/version.py
@@ -1,1 +1,1 @@
-program_version = "2025.12.12"
+program_version = "2025.12.15"

b0be1bfb83b9eeaa4ea66699aa443e61adb27442
guenthner <j@guenthner.xyz>
2026-02-15 07:37:51 +0100

    Adding graph and priority stack

---
+++ messthaler_wulff/graph.py
@@ -0,0 +1,100 @@
+import abc
+from typing import Self, Sequence
+
+import numpy as np
+
+from messthaler_wulff.decorators import hacky_instance_cache
+
+type Key = int
+type Vector = Sequence[int]
+
+
+class Graph(abc.ABC):
+    """Represent an abstract graph. Can be subclassed to create finite graphs, lattices, etc."""
+    ZERO = 0
+
+    @property
+    @abc.abstractmethod
+    def max_degree(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def neighbors(self, node: Key) -> Sequence[Key]:
+        """The index-th neighbor of the node 'node'. The index may be
+        at most one less than what 'degree' returns for this node"""
+        pass
+
+
+class UniformNeighborhood:
+    """A neighborhood of nodes in a graph that has the same degree and structure at every point"""
+
+    def __init__(self, neighbors: list[Vector]) -> None:
+        self.dimension = len(neighbors[0])
+        assert all(len(v) == self.dimension for v in neighbors)
+        self._neighbors = neighbors
+        self.degree = len(self._neighbors)
+        self.zero = tuple([0] * self.dimension)
+
+    def neighbor(self, node: Vector, index: int) -> Vector:
+        neighbor = self._neighbors[index]
+        assert len(node) == len(neighbor), f"Dimension mismatch between {node} and {neighbor}"
+
+        return tuple(node[i] + neighbor[i] for i in range(len(node)))
+
+    @classmethod
+    def from_basis(cls, basis: list[Vector]) -> Self:
+        """This function inserts the inverses of the basis vectors"""
+        return cls([*basis, *(tuple(map(lambda x: -x, v)) for v in basis)])
+
+    @classmethod
+    def from_transform(cls, transform: np.ndarray) -> Self:
+        ...
+
+
+class Lattice(Graph):
+    """A graph given by a neighborhood and all possible translations of it"""
+
+    def __init__(self, neighborhood: UniformNeighborhood) -> None:
+        self.neighborhood = neighborhood
+        self.keys: dict[Vector, Key] = {neighborhood.zero: Graph.ZERO}
+        self.values: list[Vector] = [neighborhood.zero]
+        self._neighbors: list[Sequence[Key]] = []
+
+    def intern(self, node: Vector) -> Key:
+        """Get the canonical representation of a vector for this lattice"""
+        assert len(node) == self.neighborhood.dimension
+
+        if node in self.keys:
+            return self.keys[node]
+
+        key = len(self.values)
+        self.values.append(node)
+        self.keys[node] = key
+        return key
+
+    def exists(self, node: Key) -> bool:
+        return node < len(self.values)
+
+    @property
+    def max_degree(self) -> int:
+        return self.neighborhood.degree
+
+    @hacky_instance_cache("_neighbors")
+    def neighbors(self, node: Key) -> Sequence[Key]:
+        assert isinstance(node, int)
+        assert self.exists(node)
+
+        value = self.values[node]
+        neighbors = tuple(
+            self.intern(self.neighborhood.neighbor(value, i))
+            for i in range(self.neighborhood.degree))
+
+        assert len(
+            neighbors) == self.neighborhood.degree, f"Should have {self.neighborhood.degree} neighbors, not {len(neighbors)}"
+
+        return neighbors
+
+    def walk_path(self, node: Key, indices: Sequence[int]) -> Key:
+        for i in indices:
+            node = self.neighbors(node)[i]
+        return node
---
+++ messthaler_wulff/priority_stack.py
@@ -0,0 +1,178 @@
+import textwrap
+from typing import Optional, Iterable, Callable, Iterator
+
+from messthaler_wulff.decorators import map_output
+
+type Priority = int
+type Key = int
+type PriorityLevel = list[Key]
+
+
+class defaultlist[T]:
+    def __init__(self, default: Callable[..., T]):
+        self.default: Callable[..., T] = default
+        self.values: list[T] = list()
+
+    def _ensure_capacity(self, key: int):
+        l = self.values
+        while len(l) <= key:
+            l.append(self.default())
+
+    def __len__(self):
+        return len(self.values)
+
+    def __getitem__(self, key: int) -> T:
+        self._ensure_capacity(key)
+        return self.values[key]
+
+    def __setitem__(self, key: int, value: T) -> None:
+        self._ensure_capacity(key)
+        self.values[key] = value
+
+    def __str__(self):
+        return str(self.values)
+
+
+class PriorityStack:
+    """An implementation of a priority queue optimized for energy levels"""
+
+    def __init__(self, priority_count) -> None:
+        self.min_priority: Optional[Priority] = None
+        self.priority_levels: list[PriorityLevel] = [list() for _ in range(priority_count)]
+        self.priorities: defaultlist[Optional[Priority]] = defaultlist(lambda: None)
+        self.indices: defaultlist[Optional[int]] = defaultlist(lambda: None)
+        self.size: int = 0
+
+    def minimums(self) -> Iterable[Priority]:
+        assert self.min_priority is not None
+        return self.priority_levels[self.min_priority]
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __contains__(self, key: Key) -> bool:
+        assert key >= 0
+        return self.priorities[key] is not None
+
+    def get_priority(self, key: Key) -> Priority:
+        assert key in self, f"Priority for {key} is {self.priorities[key]}"
+        return self.priorities[key]
+
+    def _remove_from_level(self, level: PriorityLevel, key: Key):
+        index = self.indices[key]
+        assert index is not None
+        assert 0 <= index < len(level), f"Failed 0 <= {index} < {len(level)}"
+        assert level[index] == key
+        self.indices[key] = None
+
+        if index == len(level) - 1:
+            level.pop()
+        else:
+            move = level.pop()
+            level[index] = move
+            self.indices[move] = index
+
+    def _add_to_level(self, level: PriorityLevel, key: Key) -> None:
+        self.indices[key] = len(level)
+        level.append(key)
+
+    def _assert_priority(self, key: Key, priority: Priority) -> None:
+        actual = self.get_priority(key)
+        assert actual == priority, f"Expected priority {priority}, got {actual} instead"
+        assert self.priority_levels[actual][self.indices[key]] == key
+
+    def _remove_priority_entry(self, key: Key, priority: Priority) -> None:
+        level = self.priority_levels[priority]
+        self._remove_from_level(level, key)
+
+    def _add_priority_entry(self, key: Key, priority: Priority) -> None:
+        level = self.priority_levels[priority]
+        self._add_to_level(level, key)
+
+    def set_priority(self, key: Key, priority: Priority) -> None:
+        assert priority < len(self.priority_levels)
+        if self.min_priority is None or priority < self.min_priority:
+            self.min_priority = priority
+
+        is_already_present = key in self
+
+        if is_already_present:
+            old_priority = self.get_priority(key)
+            if old_priority == priority:
+                return
+            self._remove_priority_entry(key, old_priority)
+        else:
+            self.size += 1
+
+        self._add_priority_entry(key, priority)
+        self.priorities[key] = priority
+
+        self._adjust_min_priority()
+
+        self._assert_priority(key, priority)
+        assert len(self) > 0
+
+    def unset_priority(self, key: Key) -> None:
+        assert key in self
+        self.size -= 1
+        priority = self.priorities[key]
+        level = self.priority_levels[priority]
+        self._remove_from_level(level, key)
+        self.priorities[key] = None
+
+        self._adjust_min_priority()
+
+    def _adjust_min_priority(self) -> None:
+        if len(self) == 0:
+            self.min_priority = None
+        else:
+            while len(self.priority_levels[self.min_priority]) <= 0:
+                self.min_priority += 1
+
+    def __iter__(self) -> Iterator[Key]:
+        count = 0
+
+        for level in self.priority_levels:
+            count += len(level)
+            yield from level
+
+        assert count == len(self)
+
+    @map_output(list)
+    def invariant_failures(self) -> Iterable[str]:
+        """Returns a list of strings of which invariants do
+        not hold on this instance"""
+
+        for key in range(len(self.priorities)):
+            priority = self.priorities[key]
+            index = self.indices[key]
+
+            if (priority is None) != (index is None):
+                yield f"Priority and index are not in sync for {key}"
+                continue
+
+            if priority is None: continue
+            if priority >= len(self.priority_levels):
+                yield (f"Priority {priority} is "
+                       f"too large for maximum "
+                       f"{len(self.priority_levels)} (key: {key})")
+                continue
+            if index >= len(self.priority_levels[priority]):
+                yield (f"Priority level {priority} "
+                       f"is too short (key: {key})")
+                continue
+
+            if self.priority_levels[priority][index] != key:
+                yield f"{self.priority_levels[priority][index]} does not match {key}"
+
+    def test_invariants(self) -> None:
+        failures = self.invariant_failures()
+        if len(failures) == 0: return
+
+        raise RuntimeError(f"Following {len(failures)} invariants where violated:\n"
+                           f"{textwrap.indent("\n".join(failures), " " * 4)}\n"
+                           f"Context:\n"
+                           f"{self.size}\n"
+                           f"{self.priorities}\n"
+                           f"{self.indices}\n"
+                           f"{self.priority_levels}")
